5f26d705546c7a02482ba2b0a31eb0a1434c0ab6
artemkosterin <160392965+artemkosterin@users.noreply.github.com>
2024-03-09 19:25:27 +0300

    b117 Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -31,7 +31,7 @@ double expn(double x, uint16_t count) {
 double sinn(double x, uint16_t count) {
   double result = 0.0;
   for (uint16_t i = 1; i <= count; i++)
-    result += pown(-1, i - 1) * calcItem(x, 2 * i - 1);
+    result = result + pown(-1, i - 1) * calcItem(x, 2 * i - 1);
   return result;
 }
 
